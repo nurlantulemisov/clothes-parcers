fc9ca8d5a7d9f3bc97ad86b72dbf2f7eb73ddeeb
Nurlan Tulemisov <nurlan.tulemisov@gmail.com>
2020-10-01 20:34:27 +0300

    Добавил парсер данных из очереди. Создание детали и демона

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import pika
 import os
 import time
+import json
+import zara
+import daemon
 
-
-def task_handle(msg):
-    print("RabbitMq processing")
-    print(" [x] Received " + str(msg))
-
-    time.sleep(5)
-    print(" RabbitMq processing finished")
+def task_handle(msg: str):
+    data = json.loads(msg)
+    item_data = data['clothes']
+    if item_data['shop'] == zara.Zara.parcer_code:
+        print('Zara process starting')
+        zara = zara.Zara(item_data['code'])
+        detail_zara = zara.run()
+        print(detail_zara)
+        print('Zara process stoping')
     return
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body: str):
     task_handle(body)
 
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://app:qwerty@0.0.0.0:5672/%2f')
-params = pika.URLParameters(url)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()  # start a channel
-channel.queue_declare(queue='parcerProcess', durable=True)
+def consume():
+    url = os.environ.get('CLOUDAMQP_URL', 'amqp://app:qwerty@0.0.0.0:5672/%2f')
+    params = pika.URLParameters(url)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()  # start a channel
+    channel.queue_declare(queue='parcerProcess', durable=True)
 
-channel.basic_consume('parcerProcess',
-                      callback,
-                      auto_ack=True)
+    channel.basic_consume('parcerProcess',
+                        callback,
+                        auto_ack=True)
 
-channel.start_consuming()
-connection.close()
+    channel.start_consuming()
+    connection.close()
+
+with daemon.DaemonContext():
+    consume()
--- zara.py
+++ zara.py
@@ -8,49 +8,57 @@ from detail.size import Size
 from detail.detail import Detail
 from detail.price import Price
 import collections
-import json
+import enum
 
-# todo заменить название файла
-def find_color(driver: webdriver) -> str:
-    color_clothe = driver.find_element_by_css_selector("._colorName")
-    return color_clothe.text
+class Zara(enum.Enum):
+    parcer_code = 1
 
-def find_sizes(driver: webdriver) -> List[Size]:
-    sizes = driver.find_elements_by_css_selector('.size-list .product-size')
-    obj_sizes = []
-    for size in sizes:
-        deque_size_types = collections.deque(size.get_attribute('data-name').split(' ('), 2)
-        obj_size = Size(deque_size_types.popleft(), clean_nubmer_size(deque_size_types.popleft()), is_disable_size(size))
-        obj_sizes.append(obj_size)
-    return obj_sizes
+    def __init__(self, item_code: str, isset_thumbnail: bool = False):
+        self.item_code = item_code
+        self.isset_thumbnail = isset_thumbnail
 
-def clean_nubmer_size(subject: str) -> str:
-    replace = subject.split(')')
-    return replace[0].split(' ')[1]
+    def find_color(self, driver: webdriver) -> str:
+        color_clothe = driver.find_element_by_css_selector("._colorName")
+        return color_clothe.text
 
-# todo убрать
-def is_disable_size(driver: webdriver) -> bool:
-    return "disabled" in driver.get_attribute("class")
-    
-def run():
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome('./driver/macOS/chromedriver', options=op)
-    driver.get("https://www.zara.com/ru/ru/search?searchTerm=5479200800")
-    driver.implicitly_wait(10)
-    product_grid = driver.find_element_by_css_selector(".product-grid")
-    thumbnail = product_grid.find_element_by_css_selector("img").get_attribute("src")
+    def find_sizes(self, driver: webdriver) -> List[Size]:
+        sizes = driver.find_elements_by_css_selector(
+            '.size-list .product-size')
+        obj_sizes = []
+        for size in sizes:
+            deque_size_types = collections.deque(
+                size.get_attribute('data-name').split(' ('), 2)
+            obj_size = Size(deque_size_types.popleft(), self.clean_nubmer_size(
+                deque_size_types.popleft()), self.is_disable_size(size))
+            obj_sizes.append(obj_size)
+        return obj_sizes
 
-    detail_information = product_grid.find_element_by_css_selector('a.product-link')
-    reference = detail_information.get_attribute('href')
+    def clean_nubmer_size(self, subject: str) -> str:
+        replace = subject.split(')')
+        return replace[0].split(' ')[1]
 
-    driver.get(reference)
-    driver.implicitly_wait(10)
+    # todo убрать
+    def is_disable_size(self, driver: webdriver) -> bool:
+        return "disabled" in driver.get_attribute("class")
 
-    #todo add price and name
-    clothe = Detail('', reference, thumbnail, find_color(driver), find_sizes(driver), [Price(1000, False)])
-    print(json.dumps(clothe.toJson()))
-    driver.close()
+    def run(self) -> Detail:
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        driver = webdriver.Chrome('./driver/macOS/chromedriver', options=op)
+        driver.get("https://www.zara.com/ru/ru/search?searchTerm=" + self.item_code) #5479200800
+        driver.implicitly_wait(10)
+        product_grid = driver.find_element_by_css_selector(".product-list")
+        thumbnail = product_grid.find_element_by_css_selector(
+            "img").get_attribute("src")
 
-if __name__ == "__main__":
-    run()
+        detail_information = product_grid.find_element_by_css_selector('a')
+        reference = detail_information.get_attribute('href')
+
+        driver.get(reference)
+        driver.implicitly_wait(10)
+
+        #todo add price and name
+        clothes = Detail('', reference, thumbnail, self.find_color(
+            driver), self.find_sizes(driver), [Price(1000, False)])
+        driver.close()
+        return clothes
