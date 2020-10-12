bbca788a46d6e4ce809d00d2e26d4dad6222b904
Egor <efimovego@gmail.com>
2020-10-12 18:00:41 +0300

    change by pep8

--- shop/levi.py
+++ shop/levi.py
@@ -1,42 +1,42 @@
-
 """
 Levi`s parcer
 """
 
-import collections
 from typing import List
 from selenium import webdriver
 from item.detail import Detail
 from item.size import Size
-from item.price import Price
 from shop.shop_interface import ShopInterface
-import settings
+
 
 class Levi(ShopInterface):
     """Levi`s parcer class"""
+
     def __init__(self, item_code: str, isset_thumbnail: bool = False):
         self.item_code = item_code
         self.isset_thumbnail = isset_thumbnail
 
-    def find_color(self, driver: webdriver) -> str:
+    @staticmethod
+    def find_color(driver: webdriver) -> str:
         clothe_color = driver.find_element_by_xpath(
             '//*[@id="pdpRootInstance"]/div/div/div[1]/div/div/div[3]/div[3]/div[2]/div/div[1]/span[2]')
         return clothe_color.get_attribute("innerHTML")
 
-    def find_sizes(self, driver: webdriver) -> List[Size]:
+    @staticmethod
+    def find_sizes(driver: webdriver) -> List[Size]:
         clothe_sizes = driver.find_element_by_css_selector("div.tiles-outer-grid-container")
         sizes_list = clothe_sizes.text
         return sizes_list.splitlines()
 
     def run(self) -> Detail:
         """Запускает задачу на сбор данных"""
-        CHROME_DRIVER_PATH = 'chromedriver/chromedriver'  # путь до драйвера в проекте
-        LEVIS = 'https://www.levi.com/RU/ru_RU/'  # сайт для парсинга
+        chrome_driver_path = 'chromedriver/chromedriver'  # путь до драйвера в проекте
+        levis = 'https://www.levi.com/RU/ru_RU/'  # сайт для парсинга
         # item_code = 'p/269860002'
         options = webdriver.ChromeOptions()
         options.add_argument('headless')  # для браузера
-        driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=options)
-        driver.get(LEVIS + self.item_code)
+        driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
+        driver.get(levis + self.item_code)
         driver.implicitly_wait(10)
         reference = driver.current_url
         product_img_list = driver.find_element_by_class_name('image-gallery')
